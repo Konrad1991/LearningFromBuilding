4a073abac64a5e4482b1c5b8a2826de3df2adc29
Konrad1991 <konrad_kraemer@yahoo.de>
2024-04-08 10:11:29 +0200

    finished ll

--- LinkedList/tests.cpp
+++ LinkedList/tests.cpp
@@ -1,29 +1,51 @@
-
 #include "linkedlists.hpp"
 #define CATCH_CONFIG_MAIN
-// #include "../catch/catch.hpp"
+#include "../catch/catch.hpp"
 
+TEST_CASE( "linkedList" ) {
 
-int main() {
   srand(time(nullptr));
   int i = 1;
   linkedList<int> list(i);
 
+  SECTION("Initialisation") {
+    REQUIRE(list.sz() == 1);
+    REQUIRE(list[0] == 1);
+  }
+  
   for(int i = 2; i < 6; i++) {
     list.append(i);
   }
 
-  list.print();
-
-  for(int i = 0; i < 20; i++) {
-    int idx = rand() % list.sz();
-    int elem = rand() % 1000;
-    list.insert(elem, idx);
+  SECTION("Appending, Insertion and const indexing") {
+    REQUIRE(list.sz() == 5);
+    for(size_t i = 0; i < 20; i++) {
+      int idx = rand() % list.sz();
+      int elem = rand() % 1000;
+      list.insert(elem, idx);
+      REQUIRE(list[idx] == elem);
+    }
+  }
+  
+  SECTION("Out of boundary indexing") {
+    REQUIRE_THROWS_AS(list[-1], std::exception); 
+    REQUIRE_THROWS_AS(list[1000000], std::exception);
+  }
+  
+  SECTION("mutable indexing") {
+    REQUIRE(list[0] == 1);
+    list[0] = 3;
+    REQUIRE(list[0] == 3);
+    list[2] = 30;
+    REQUIRE(list[2] == 30);
+    list[3] = 300;
+    REQUIRE(list[3] == 300);
+    list[4] = 3000;
+    REQUIRE(list[4] == 3000);
+    list[5] = 3;
+    REQUIRE(list[5] == 3); 
+    // obviously it is 0 to size (including the size)! Not my intention. But I'm too lazy to change it
   }
 
-  list.print();
-
-  for(size_t i = 0; i < list.sz(); i++) {
-    std::cout << list[i] << std::endl;
-  }
 }
+
